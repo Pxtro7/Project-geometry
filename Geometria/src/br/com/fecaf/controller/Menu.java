b84659b133ad3fe76264e7fcda82ed4256f30989
Vinicius <vinicius.pxtro@gmail.com>
2025-03-26 23:03:52 -0300

    Geometria

---
+++ Geometria/src/br/com/fecaf/controller/Menu.java
@@ -0,0 +1,184 @@
+package br.com.fecaf.controller;
+
+import br.com.fecaf.model.Circulo;
+import br.com.fecaf.model.Retangulo;
+import br.com.fecaf.model.Triangulo;
+
+import java.util.Scanner;
+
+public class Menu {
+
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void executarMenu() {
+
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("/*/");
+            System.out.println("/*   Geometria   *");
+            System.out.println("/*/");
+            System.out.println("/* 1 - Circulo   *");
+            System.out.println("/* 2 - Retangulo *");
+            System.out.println("/* 3 - Triangulo *");
+            System.out.println("/* 4 - Sair      *");
+            System.out.println("/*/");
+
+            System.out.println("Informe a opção desejada: ");
+            int optionUser = scanner.nextInt();
+
+            switch(optionUser) {
+                case 1:
+
+                    boolean exitCirculo = false;
+
+                    Circulo circulo = new Circulo();
+
+                    boolean validaCadastro = false;
+
+                    while (!exitCirculo) {
+                        System.out.println("/*/");
+                        System.out.println("/******** Circulo  *********/");
+                        System.out.println("/*/");
+                        System.out.println("/* 1 - Cadastrar circulo   */");
+                        System.out.println("/* 2 - Calcular area       */");
+                        System.out.println("/* 3 - Calcular perimetro  */");
+                        System.out.println("/* 4 - Sair                */");
+                        System.out.println("/*/");
+                        System.out.println("Informe a opção desejada:");
+
+                        int optionCirculo = scanner.nextInt();
+
+                        switch (optionCirculo) {
+                            case 1:
+                                validaCadastro = circulo.cadastrarCirculo();
+                                break;
+                            case 2:
+                                if (validaCadastro) {
+                                    circulo.calcularArea();
+                                } else {
+                                    System.out.println("Cadastre um circulo: ");
+                                }
+                                break;
+                            case 3:
+                                if (validaCadastro) {
+                                    circulo.calcularPerimetro();
+                                } else {
+                                    System.out.println("Cadastre um circulo: ");
+                                }
+                                break;
+                            case 4:
+                                System.out.println("Saindo do circulo...");
+                                exitCirculo = true;
+                                break;
+                            default:
+                                System.out.println("ESCOLHA UMA OPÇÃO VÁLIDA");
+                        }
+                    }
+                    break;
+                case 2:
+
+                    boolean exitRetangulo = false;
+
+                    Retangulo retangulo = new Retangulo();
+
+                    boolean validaRetangulo = false;
+
+                    while (!exitRetangulo) {
+                        System.out.println("/***************************/");
+                        System.out.println("/******** Retangulo  *******/");
+                        System.out.println("/***************************/");
+                        System.out.println("/* 1 - Cadastrar Retangulo */");
+                        System.out.println("/* 2 - Calcular area       */");
+                        System.out.println("/* 3 - Calcular perimetro  */");
+                        System.out.println("/* 4 - Sair                */");
+                        System.out.println("/***************************/");
+                        System.out.println("Informe a opção desejada:");
+
+                        int optionRetangulo = scanner.nextInt();
+
+                        switch (optionRetangulo) {
+                            case 1:
+                                validaRetangulo = retangulo.cadastrarRetangulo();
+                                break;
+                            case 2:
+                                if (validaRetangulo) {
+                                    retangulo.calcularArea();
+                                } else {
+                                    System.out.println("Cadastre um Retangulo");
+                                }
+                                break;
+                            case 3:
+                                if (validaRetangulo) {
+                                    retangulo.calcularArea();
+                                } else {
+                                    System.out.println("Cadastre um Retangulo");
+                                }
+                                break;
+                            case 4:
+                                System.out.println("Saindo Retangulo...");
+                                exitRetangulo = true;
+                                break;
+                            default:
+                                System.out.println("Escolha uma opção valida! ");
+                        }
+                    }
+                    break;
+                case 3:
+
+                    boolean exitTriangulo = false;
+
+                    Triangulo triangulo = new Triangulo();
+
+                    boolean validaTriangulo = false;
+
+                    while (!exitTriangulo) {
+                        System.out.println("/**********************************/");
+                        System.out.println("/********    Triangulo  ***********/");
+                        System.out.println("/**********************************/");
+                        System.out.println("/* 1 - Cadastrar triangulo        */");
+                        System.out.println("/* 2 - Verificar triangulo        */");
+                        System.out.println("/* 3 - Verificar Triangulo 3,4,5  */");
+                        System.out.println("/* 4 - Sair                       */");
+                        System.out.println("/**********************************/");
+                        System.out.println("Informe a opção desejada:");
+
+                        int optionTriangulo = scanner.nextInt();
+
+                        switch (optionTriangulo) {
+                            case 1:
+                                validaTriangulo = triangulo.cadastrarTriangulo();
+                                break;
+                            case 2:
+                                if (validaTriangulo) {
+                                    triangulo.classificarTriangulo();
+                                } else {
+                                    System.out.println("Cadastre um Triangulo");
+                                }
+                                break;
+                            case 3:
+                                if (validaTriangulo) {
+                                    triangulo.verificaPitagoras();
+                                } else {
+                                    System.out.println("Cadastre um Triangulo");
+                                }
+                                break;
+                            case 4:
+                                System.out.println("Saindo triangulo...");
+                                exitTriangulo = true;
+                                break;
+                        }
+                    }
+                case 4:
+                    System.out.println("SAINDO...");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Escolha uma opção válida");
+            }
+        }
+
+    }
+}
+
---
+++ Geometria/src/br/com/fecaf/model/Triangulo.java
@@ -0,0 +1,69 @@
+package br.com.fecaf.model;
+
+import java.util.Scanner;
+
+public class Triangulo {
+
+    public double ladoA;
+    public double ladoB;
+    public double ladoC;
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    //Solicita ao usuário que digite os lados do triângulo.
+    public boolean cadastrarTriangulo() {
+        System.out.println("Digite o valor do lado A do triângulo:");
+        this.ladoA = scanner.nextDouble();
+
+        System.out.println("Digite o valor do lado B do triângulo:");
+        this.ladoB = scanner.nextDouble();
+
+        System.out.println("Digite o valor do lado C do triângulo:");
+        this.ladoC = scanner.nextDouble();
+        return true;
+    }
+
+    //Verifica se é um triangulo retangulo
+    public boolean trianguloRetangulo() {
+        if (Math.pow(ladoA, 2) + Math.pow(ladoB, 2) == Math.pow(ladoC, 2)) {
+            return true;
+        }
+        if (Math.pow(ladoA, 2) + Math.pow(ladoC, 2) == Math.pow(ladoB, 2)) {
+            return true;
+        }
+        if (Math.pow(ladoB, 2) + Math.pow(ladoC, 2) == Math.pow(ladoA, 2))
+        {
+            return true;
+        }
+        return false;
+    }
+    public void classificarTriangulo(){
+        if (this.trianguloRetangulo()){
+            System.out.println("É um triangulo retangulo !!");
+        }else {
+            System.out.println("Não é um Triangulo retangulo !!");
+        }
+
+    }
+
+    public void verificaPitagoras(){
+        if (this.verificarTriangulo()){
+            System.out.println("É um triangulo 3 4 5 !!");
+        }else {
+            System.out.println("Não é um Triangulo 3 4 5 !!");
+        }
+    }
+
+
+    //verifica se é um triangulo com os lados de medida 3 4 5
+    public boolean verificarTriangulo() {
+        return (ladoA == 3 && ladoB == 4 && ladoC == 5) ||
+                (ladoA == 3 && ladoB == 5 && ladoC == 4) ||
+                (ladoA == 4 && ladoB == 3 && ladoC == 5) ||
+                (ladoA == 4 && ladoB == 5 && ladoC == 3) ||
+                (ladoA == 5 && ladoB == 3 && ladoC == 4) ||
+                (ladoA == 5 && ladoB == 4 && ladoC == 3);
+
+    }
+}
